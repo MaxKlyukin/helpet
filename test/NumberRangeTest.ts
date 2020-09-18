2cd4acf70c998db8579f5712d1b19a0410cd8f9b
MaxKlyukin <maxklukin@gmail.com>
2020-09-18 22:06:48 +0300

    add repo ling and few tests for number range

--- test/NumberRangeTest.ts
+++ test/NumberRangeTest.ts
@@ -1,8 +1,8 @@
 "use strict";
 
 import {expect} from "chai";
-import {NumberRange, NumberRangeException} from "../src/NumberRange";
-import {List} from "../src/type/List";
+import {NumberRange, NumberRangeException, range} from "../src";
+import {List} from "../src";
 
 describe('NumberRange', () => {
 
@@ -15,6 +15,13 @@ describe('NumberRange', () => {
 
         return expect(range.start).to.be.eq(10)
             && expect(range.end).to.be.eq(11)
+    });
+
+    it('can be created using helper function', function () {
+        const r = range(10);
+
+        return expect(r.start).to.be.eq(0)
+            && expect(r.end).to.be.eq(9)
     });
 
     it('knows if it is singular', function () {
@@ -71,12 +78,24 @@ describe('NumberRange', () => {
         const range = new NumberRange(99, 103);
 
         const iteratedNumbers = [];
-        for(const number of range.numbers()) {
+        for (const number of range.numbers()) {
             iteratedNumbers.push(number);
         }
 
         return expect(iteratedNumbers).to.have.lengthOf(5)
             && expect(iteratedNumbers).to.be.deep.equal([99, 100, 101, 102, 103])
+    });
+
+    it('can be iterated over itself', function () {
+        const range = new NumberRange(69, 77);
+
+        const iteratedNumbers = [];
+        for (const number of range.numbers()) {
+            iteratedNumbers.push(number);
+        }
+
+        return expect(iteratedNumbers).to.have.lengthOf(9)
+            && expect(iteratedNumbers).to.be.deep.equal([69, 70, 71, 72, 73, 74, 75, 76, 77])
     });
 
     it('can be turned into List', function () {
